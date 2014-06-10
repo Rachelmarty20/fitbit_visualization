b850f3c71db0a219054e3ebe6edae6a9467e8b51
Rachelmarty20 <Rachelmarty20@gmail.com>
2014-06-09 22:48:27 -0700

    adding print statement

--- try.py
+++ try.py
@@ -6,6 +6,8 @@ import oauth2
 consumer = oauth2.Consumer(key=FITBIT_APP_KEY, secret=FITBIT_APP_SECRET)
 client = oauth2.Client(consumer)
 resp, content = client.request('http://api.fitbit.com/oauth/request_token', 'GET')
+print resp
+print content
 token = oauth2.Token.from_string(content)
 # NOTE: the auth URL uses www.fitbit.com as the domain, NOT api.fitbit.com
 auth_url = 'http://www.fitbit.com/oauth/authorize?oauth_token={0}'.format(token.key)
